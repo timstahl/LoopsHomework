b60c3ec35689667e8c62da9baa093b0b24daa589
Tim Stahl <timothystahl@gmail.com>
2020-01-01 13:58:32 -0700

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please enter a list of names, seperate each one with a comma: ");
+            string namesList = Console.ReadLine();
+            
+            string[] words = namesList.Split(",");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine($"Hello { words[i] }");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
